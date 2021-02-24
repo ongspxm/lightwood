5e72b7c2957943a9b67206e3a07deca31fc88c30
Natasha Seelam <nseelam1@gmail.com>
2021-02-24 10:53:29 -0500

    change tokenizer to class

--- lightwood/helpers/tokenizer.py
+++ lightwood/helpers/tokenizer.py
@@ -35,54 +35,86 @@ import sys
 #    !{sys.executable} -m spacy download en ## FIXME This is pretty hideous
 #    nlp = spacy.load('en', disable=['parser', 'ner'])
 
-
-nlp = spacy.load('en', disable=['parser', 'ner'])
-stop_words = nlp.Defaults.stop_words
-
-def strip_character(phrase):
+class Tokenizer():
     """
-    Omit non-alphanumeric characters for string input.
+    Tokenization class.
 
-    Preserves spaces in text and keeps number
+    A model that intakes strings, and converts the text
+
+    Args:
+    ::param (str, defaults to 'en'); the language to tokenize in.
     """
-    return re.sub("\W+", " ", phrase)
+    def __init__(self, lang='en'):
 
+        if lang == 'en':
+            self.nlp = spacy.load('en', disable=['parser', 'ner'])
+            self.stop_words = self.nlp.Defaults.stop_words
+        else:
+            raise Exception('English only language supported')
 
-def omit_numbers(phrase):
-    """ Remove numbers; phrase is full string """
-    return "".join([c.lower() for c in phrase if not c.isdigit()])
+    def encode(self, phrase):
+        """
+        Given a phrase, tokenize the text.
+        Make sure character stripping is last.
+        """
+        # Enforce lowercase
 
+        # Remove strange text
+        text = self.omit_numbers(phrase.lower())
 
-def remove_stop(phrase):
-    """ Omits stop words from phrase """
-    return " ".join([tok.text for tok in nlp(phrase) if tok.text not in stop_words])
+        # Remove numbers
+        text = self.remove_stop(text, self.nlp, self.stop_words)
 
+        # Omit Stop words
+        text = self.strip_character(text)
 
-def lemmatize(phrase):
-    """
-    Given a str, convert to lemmatized tokens.
-    Returns list of strings
-    """
-    return [token.lemma_ for token in nlp(phrase)]
+        # Lemmatize
+        text = self.lemmatize(text, self.nlp)
 
+        return text
 
-def tokenize(phrase):
-    """
-    Given a phrase, tokenize the text.
-    Make sure character stripping is last.
-    """
-    # Enforce lowercase
+    @staticmethod
+    def strip_character(phrase):
+        """
+        Omit non-alphanumeric characters for string input.
 
-    # Remove strange text
-    text = omit_numbers(phrase.lower())
+        Args:
+        ::param phrase (str) - input phrase
 
-    # Remove numbers
-    text = remove_stop(text)
+        Returns string without special symbols
+        """
+        return re.sub("\W+", " ", phrase)
 
-    # Omit Stop words
-    text = strip_character(text)
+    @staticmethod
+    def omit_numbers(phrase):
+        """ Remove numbers; phrase is full string """
+        return "".join([c.lower() for c in phrase if not c.isdigit()])
 
-    # Lemmatize
-    text = lemmatize(text)
+    @staticmethod
+    def remove_stop(phrase, nlp, stop_words):
+        """
+        Omits stop words from phrase
 
-    return text
+        Args:
+        ::param phrase (str) - input phrase
+        ::param nlp (spacy.lang model) - defaults to English
+        ::param stop_words (set of str) - which words to omit. May be customized.
+
+        Returns str without stop-words
+        """
+        return " ".join([tok.text for tok in nlp(phrase) if tok.text not in stop_words])
+
+    @staticmethod
+    def lemmatize(phrase, nlp):
+        """
+        Given a str, convert to lemmatized tokens.
+        Returns list of strings
+
+        Args:
+        ::param phrase (str) - input phrase
+        ::param nlp (spacy.lang model) - defaults to English
+
+        Returns tokenized list of strings
+
+        """
+        return [token.lemma_.text for token in nlp(phrase)]
