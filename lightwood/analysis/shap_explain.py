915a851af10198adc20ac7f42047754560870484
Patricio Cerda Mardini <pcerdam@live.com>
2021-09-14 20:10:36 -0300

    refactor: simplify shap analyzer and explainer

--- lightwood/analysis/shap_explain.py
+++ lightwood/analysis/shap_explain.py
@@ -1,46 +1,24 @@
-from copy import deepcopy
-
+import shap
 import torch
-import numpy as np
 import pandas as pd
 
-# restore_icp_state, clear_icp_state
-from lightwood.analysis.nc.util import get_numerical_conf_range, get_categorical_conf, get_anomalies
-from lightwood.helpers.ts import get_inferred_timestamps, add_tn_conf_bounds
 from lightwood.api.dtype import dtype
-from lightwood.api.types import TimeseriesSettings
 from lightwood.ensemble import BestOf
-import shap
 
 
 def shap_explain(data: pd.DataFrame,
             encoded_data: torch.Tensor,
             predictions: pd.DataFrame,
             ensemble: BestOf,
-            timeseries_settings: TimeseriesSettings,
-            analysis: dict,
             target_name: str,
             target_dtype: str,
-            positive_domain: bool,
-            fixed_confidence: float,
-            anomaly_detection: bool,
-
-            # forces specific confidence level in ICP
-            anomaly_error_rate: float,
-
-            # ignores anomaly detection for N steps after an
-            # initial anomaly triggers the cooldown period;
-            # implicitly assumes series are regularly spaced
-            anomaly_cooldown: int,
-
-            ts_analysis: dict = None
             ):
 
     data = data.reset_index(drop=True)
-
     explainer = shap.Explainer(ensemble.models[ensemble.best_index].model)
-    shap_values = explainer(encoded_data)
-    shap.plots.waterfall(shap_values[0])
+    shap_values = explainer.shap_values(encoded_data).toarray()
+    shap_df = pd.DataFrame(shap_values,
+                           columns=[f"feature_{i}_impact" for i in range(shap_values.shape[1])])
 
     insights = pd.DataFrame()
     if target_name in data.columns:
@@ -56,5 +34,11 @@ def shap_explain(data: pd.DataFrame,
         insights['prediction'] = insights['prediction'].astype(float)
     elif target_dtype in (dtype.short_text, dtype.rich_text, dtype.binary, dtype.categorical):
         insights['prediction'] = insights['prediction'].astype(str)
+
+    # Merge SHAP values into final dataframe
+    insights = pd.concat([insights, shap_df], axis=1)
+
+    # Add model base response
+    insights['base_response'] = insights.apply(lambda x: x['prediction'] - sum([x[f'feature_{i}_impact'] for i in range(36)]), axis=1)
 
     return insights
--- lightwood/analysis/shap_model_analyzer.py
+++ lightwood/analysis/shap_model_analyzer.py
@@ -1,26 +1,12 @@
 from typing import Dict, List
 
-import torch
-import numpy as np
 import pandas as pd
-from copy import deepcopy
-from itertools import product
-from sklearn.preprocessing import OneHotEncoder
 
 from lightwood.api import dtype
-from lightwood.api.types import ModelAnalysis, StatisticalAnalysis, TimeseriesSettings
+from lightwood.api.types import ModelAnalysis, StatisticalAnalysis
 from lightwood.data.encoded_ds import ConcatedEncodedDs, EncodedDs
-from lightwood.helpers.general import evaluate_accuracy
 from lightwood.ensemble import BaseEnsemble
-from lightwood.encoder.text.pretrained import PretrainedLangEncoder
-
 from lightwood.analysis.acc_stats import AccStats
-from lightwood.analysis.nc.norm import Normalizer
-from lightwood.analysis.nc.nc import BoostedAbsErrorErrFunc
-from lightwood.analysis.nc.util import clean_df, set_conf_range
-from lightwood.analysis.nc.icp import IcpRegressor, IcpClassifier
-from lightwood.analysis.nc.nc import RegressorNc, ClassifierNc, MarginErrFunc
-from lightwood.analysis.nc.wrappers import ConformalClassifierAdapter, ConformalRegressorAdapter, t_softmax
 
 
 def shap_model_analyzer(
@@ -29,30 +15,17 @@ def shap_model_analyzer(
         train_data: List[EncodedDs],
         stats_info: StatisticalAnalysis,
         target: str,
-        ts_cfg: TimeseriesSettings,
         dtype_dict: Dict[str, str],
-        disable_column_importance: bool,
-        fixed_significance: float,
-        positive_domain: bool,
-        confidence_normalizer: bool,
-        accuracy_functions
 ):
-    runtime_analyzer = {}
-
     encoded_train_data = ConcatedEncodedDs(train_data)
     encoded_data = ConcatedEncodedDs(data)
 
     data_type = dtype_dict[target]
-    data_subtype = data_type
 
     is_numerical = data_type in [dtype.integer, dtype.float] or data_type in [dtype.array]
     is_classification = data_type in (dtype.categorical, dtype.binary)
-    is_multi_ts = ts_cfg.is_timeseries and ts_cfg.nr_predictions > 1
 
     data = encoded_data.data_frame
-    runtime_analyzer = {}
-    predictions = {}
-    input_cols = list([col for col in data.columns if col != target])
     normal_predictions = predictor(encoded_data) if not is_classification else predictor(
         encoded_data, predict_proba=True)
     normal_predictions = normal_predictions.set_index(data.index)
@@ -63,7 +36,6 @@ def shap_model_analyzer(
     acc_stats.fit(data, normal_predictions, conf=result_df)
     bucket_accuracy, accuracy_histogram, cm, accuracy_samples = acc_stats.get_accuracy_stats(
         is_classification=is_classification, is_numerical=is_numerical)
-    runtime_analyzer['bucket_accuracy'] = bucket_accuracy
 
     model_analysis = ModelAnalysis(
         accuracies={},
@@ -76,5 +48,4 @@ def shap_model_analyzer(
         histograms=stats_info.histograms,
         dtypes=dtype_dict
     )
-
-    return model_analysis, runtime_analyzer
+    return model_analysis, {}
