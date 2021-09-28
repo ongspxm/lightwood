c18edc7c4bc6b886f319634635ab7f09ace44dfd
Patricio Cerda Mardini <pcerdam@live.com>
2021-09-28 17:50:34 -0300

    fix: imports

--- lightwood/analysis/__init__.py
+++ lightwood/analysis/__init__.py
@@ -1,6 +1,5 @@
 from lightwood.analysis.analyze import model_analyzer
 from lightwood.analysis.explain import explain
-from lightwood.analysis.shap_explain import shap_explain
-from lightwood.analysis.shap_analyzer import shap_model_analyzer
+from lightwood.analysis.shap_analyzer import shap_analyzer_block as shap_analyzer
 
-__all__ = ['model_analyzer', 'shap_analyzer.py', 'explain', 'shap_explain']
+__all__ = ['model_analyzer', 'shap_analyzer', 'explain']
